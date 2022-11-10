3b9734ab4a01d691990b2db5025dc415ac9abffb
XE <2304781849@qq.com>
2022-11-10 20:22:54 +0800

    hot-fix commit6

--- src/main/java/com/xe/git/GitTest.java
+++ src/main/java/com/xe/git/GitTest.java
@@ -5,5 +5,11 @@ public class GitTest {
         System.out.println("hello Git");
         System.out.println("hello Git2");
         System.out.println("hello Git3");
+        System.out.println("hello Git4");
+        System.out.println("hello Git5");
+        System.out.println("hello Git6");
+        System.out.println("hello Git7");
+        System.out.println("hello Git8");
+        System.out.println("hello Git9");
     }
 }
